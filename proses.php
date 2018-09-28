d2230b07431fc36f510879026844e2af5685b2a6
Deshinta Rizqa Rahmawati <43508097+DeshintaRizqaR@users.noreply.github.com>
2018-09-28 10:07:27 +0700

    proses
    Memilih lebih dari 1 hobi menggunakan array

--- proses.php
+++ proses.php
@@ -1,4 +1,35 @@
-<?php
+<table>
+	<tr>
+		<td> Hobi : </td>
+	</tr>
+</table>
+<table>
+	<tr>
+		<td></td>
+			<input type="checkbox" name="Hobi[]" value="Bersepeda">Bersepeda</input><br>
+			<input type="checkbox" name="Hobi[]" value="Berenang">Berenang</input><br>
+			<input type="checkbox" name="Hobi[]" value="Nonton Film">Nonton Film</input><br>
+			<input type="checkbox" name="Hobi[]" value="Travelling">Travelling</input><br>
+			<input type="submit" name="kirim">
+			<input type="reset" name="delete" value="delete">
+	</tr>
+	<tr>
+		<td></td>
+		<td></td>
+	</tr>			
+</table>
 
 
-?>
+
+
+
+<?php 
+//if (isset($_POST['kirim'])) {
+	//$data = $_POST["Hobi"];
+	//foreach ($data as $nilai) 
+		
+	//}
+//}
+
+
+ ?>
